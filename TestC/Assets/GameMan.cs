60236ff065360a2f6b07546d8b82a77a88aad520
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-05-16 20:14:05 -0400

    Include scripts for text

---
+++ TestC/Assets/GameMan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMan : MonoBehaviour {
+    // Placeholder captions
+    string[] steps = {"zero", "one", "two", "three"};
+    int index = 0;
+    public Text msg;
+
+    public void NextStep() {
+        msg.text = steps[index++];
+        if (index == steps.Length) {
+            index = 0;
+        }
+    }
+
+    public void EndGame() {
+        Debug.Log("Game Over");
+    }
+}
