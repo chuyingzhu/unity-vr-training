e695c8dcd4af99d759b714c8a47ebc3d6c380a07
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-05-19 14:48:47 -0400

    Controller input file

---
+++ TestC/ViveInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour {
+    [SteamVR_DefaultAction("Squeeze")]
+
+    public SteamVR_Action_Single squeezeAction;
+    public SteamVR_Action_Vector2 touchPadAction;
+
+     // Update is called once per frame
+    void Update() {
+        if (SteamVR_Input.default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any())) {
+            Debug.Log("TP down");
+        }
+        if (SteamVR_Input.default.inActions.GrabPinch.GetStateUp(SteamVR_Input_Sources.Any)) {
+            Debug.Log("Grab pinch up");
+        }
+
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+        if (triggerValue > 0.0f) {
+            Debug.Log(triggerValue);
+        }
+
+        Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+        if (touchpadValue != Vector2.zero) {
+            Debug.Log(touchpadValue);
+        }
+    }
+}
+https://mega.nz/#!ryp1jCjS!bxsCcALL4m6Ft6LjtjxUuLlZG7Mb7r_huiWhl1E7OKM
