80e6ad8794baf60e2620244993321d8a2ab156c5
VRulez <50558647+VRulez@users.noreply.github.com>
2019-05-19 13:18:51 -0400

    Adding new test program for touchpad

---
+++ TouchPadTesting/Assets/_Locomotion/Scripts/VRController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRController : MonoBehaviour
+{
+    public float m_Sensitivity = 0.1f;
+    public float m_MaxSpeed = 1.0f;
+
+    // Need to change when KAT dev kit is here
+    public SteamVR_Action_Boolean m_MovePress = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    float m_Speed = 0.0f;
+    CharacterController m_CharacterController = null;
+    Transform m_CameraRig = null;
+    Transform m_Head = null;
+
+    void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CameraRig = SteamVR_Render.Top().origin;
+        m_Head = SteamVR_Render.Top().head;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandelHeight();
+        HandelHead();
+        CalculateMovement();
+    }
+
+    void HandelHead()
+    {
+        // Store Current
+        Vector3 oldPosition = m_CameraRig.position;
+        Quaternion oldRotation = m_CameraRig.rotation;
+
+        // Rotation
+        transform.eulerAngles = new Vector3(0.0f, m_Head.rotation.eulerAngles.y, 0.0f);
+
+        // Restore
+        m_CameraRig.position = oldPosition;
+        m_CameraRig.rotation = oldRotation;
+    }
+
+    void CalculateMovement()
+    {
+        // Movement Orientation
+        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Vector3 movement = Vector3.zero;
+
+        // If not moving
+        if (m_MovePress.GetStateUp(SteamVR_Input_Sources.Any))
+            m_Speed = 0;
+        
+        // If button pressed
+        if (m_MovePress.state)
+        {
+            // Add, clamp
+            m_Speed += m_MoveValue.axis.y * m_Sensitivity;
+            m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
+
+            // Orientation
+            movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+        }
+        // Apply
+        m_CharacterController.Move(movement);
+    }
+
+    void HandelHeight()
+    {
+        // Get thehead in local space
+        float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+        m_CharacterController.height = headHeight;
+
+        // Cut in half
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = m_CharacterController.height / 2;
+        newCenter.y += m_CharacterController.skinWidth;
+
+        //Move Capsule in local space
+        newCenter.x = m_Head.localPosition.x;
+        newCenter.z = m_Head.localPosition.z;
+
+        // Rotate
+        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+
+        // Apply
+        m_CharacterController.center = newCenter;
+    }
+
+
+}
