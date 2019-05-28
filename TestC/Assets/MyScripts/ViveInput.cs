3b47dd60380ef2260f1eda0c53f9407b6da036b4
VRulez <50558647+VRulez@users.noreply.github.com>
2019-05-28 18:52:22 -0400

    Locomotion successfully implemented

--- TestC/Assets/MyScripts/ViveInput.cs
+++ TestC/Assets/MyScripts/ViveInput.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 using Valve.VR;
 
 public class ViveInput : MonoBehaviour {
-    [SteamVR_DefaultAction("Squeeze")]
+    //[SteamVR_DefaultAction("Squeeze")]
 
     public SteamVR_Action_Single squeezeAction;
     public SteamVR_Action_Vector2 touchPadAction;
 
      // Update is called once per frame
     void Update() {
-        /* 
+        /*
         if (SteamVR_Actions._default.Teleport.GetStateDown(SteamVR_Input_Sources.Any)) {
             Debug.Log("TP down");
         }
         if (SteamVR_Actions._default.GrabPinch.GetStateUp(SteamVR_Input_Sources.Any)) {
             Debug.Log("Grab pinch up");
-        }*/
-
+        }
+        */
 
         float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
         if (triggerValue > 0.0f) {
