d5bc8e6722d51cdafde267f396528805e232de89
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-05-28 21:01:06 -0400

    Adding controller haptics scene

---
+++ TestC/Assets/MyScripts/Haptics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Haptics : MonoBehaviour {
+    public SteamVR_Action_Vibration hapticAction;
+    public SteamVR_Action_Boolean trackpadAction;
+
+    // Update is called once per frame
+    void Update() {
+        if (trackpadAction.GetStateDown(SteamVR_Input_Sources.LeftHand)) {
+            Pulse(1, 150, 75, SteamVR_Input_Sources.LeftHand);
+        }
+        if (trackpadAction.GetStateDown(SteamVR_Input_Sources.RightHand)) {
+            Pulse(1, 150, 75, SteamVR_Input_Sources.RightHand);
+        }
+    }
+
+    private void Pulse(float duration, float amplitude, SteamVR_Input_Sources source) {
+        hapticAction.Execute(0, duration, frequency, amplitude, source);
+        Debug.Log("Pulse " + source.ToString());
+    }
+}
