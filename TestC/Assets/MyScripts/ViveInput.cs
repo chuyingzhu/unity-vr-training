37f2546a5944174d60599c014725abbdb34a5cc4
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-05-30 14:36:22 -0400

    Master input receiver

--- TestC/Assets/MyScripts/ViveInput.cs
+++ TestC/Assets/MyScripts/ViveInput.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using Valve.VR;
 
 public class ViveInput : MonoBehaviour {
-    //[SteamVR_DefaultAction("Squeeze")]
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean teleportAction;
+    public SteamVR_Action_Boolean grabAction;
 
-    public SteamVR_Action_Single squeezeAction;
-    public SteamVR_Action_Vector2 touchPadAction;
+    public bool GetTeleportDown() {
+        return teleportAction.GetStateDown(handType);
+    }
 
-     // Update is called once per frame
+    public bool GetGrab() {
+        return grabAction.GetState(handType);
+    }
+
     void Update() {
         /*
         if (SteamVR_Actions._default.Teleport.GetStateDown(SteamVR_Input_Sources.Any)) {
@@ -18,7 +24,6 @@ public class ViveInput : MonoBehaviour {
         if (SteamVR_Actions._default.GrabPinch.GetStateUp(SteamVR_Input_Sources.Any)) {
             Debug.Log("Grab pinch up");
         }
-        */
 
         float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
         if (triggerValue > 0.0f) {
@@ -28,6 +33,15 @@ public class ViveInput : MonoBehaviour {
         Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
         if (touchpadValue != Vector2.zero) {
             Debug.Log(touchpadValue);
+        }
+        */
+
+        if (GetTeleportDown()) {
+            print("Teleport " + handType);
+        }
+
+        if (GetGrab()) {
+            print("Grab " + handType);
         }
     }
 }
