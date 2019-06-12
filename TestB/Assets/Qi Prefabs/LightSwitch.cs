605544de5f18a1c1f37e0582a1ed7440cbf5a89c
qikiDT02 <50593685+qikiDT02@users.noreply.github.com>
2019-06-12 18:44:49 -0400

    Fixed Light Switch

---
+++ TestB/Assets/Qi Prefabs/LightSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+	public UnityEngine.Events.UnityEvent BombsToTrigger;
+
+	void update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			BombsToTrigger.Invoke();
+		}
+			
+	}
+
+}
---
+++ TestB/Assets/SteamVR/InteractionSystem/Core/Scripts/LightManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+	public Light m_light;
+
+	public void turnOn() {
+		m_light.enabled = true;
+	}
+
+	public void turnOff() {
+		m_light.enabled = false;
+	}
+}
