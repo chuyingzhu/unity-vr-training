ef4bc25f7d6ca273b6cc9eafd468618314159da8
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-06-13 15:03:24 -0400

    Add liquid manager script

---
+++ KAT WALK/Assets/Scripts/LiquidManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidManager : MonoBehaviour {
+    Material m_Material;
+
+    // Start is called before the first frame update
+    void Start() {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.shader._FillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
