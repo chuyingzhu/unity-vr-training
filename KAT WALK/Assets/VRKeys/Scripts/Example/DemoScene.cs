b0edc8bb9024e2e9a0a88b79996935463f6eca76
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-06-27 20:36:47 -0400

    Start to customize keyboard

--- KAT WALK/Assets/VRKeys/Scripts/Example/DemoScene.cs
+++ KAT WALK/Assets/VRKeys/Scripts/Example/DemoScene.cs
@@ -1,36 +1,12 @@
-﻿/**
- * Copyright (c) 2017 The Campfire Union Inc - All Rights Reserved.
- *
- * Licensed under the MIT license. See LICENSE file in the project root for
- * full license information.
- *
- * Email:   info@campfireunion.com
- * Website: https://www.campfireunion.com
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 using System.Text.RegularExpressions;
 using System.Collections;
 
 namespace VRKeys {
-
-	/// <summary>
-	/// Example use of VRKeys keyboard.
-	/// </summary>
 	public class DemoScene : MonoBehaviour {
-
-		/// <summary>
-		/// Reference to the VRKeys keyboard.
-		/// </summary>
 		public Keyboard keyboard;
-
-		/// <summary>
-		/// See the following for why this is so convoluted:
-		/// http://referencesource.microsoft.com/#System.ComponentModel.DataAnnotations/DataAnnotations/EmailAddressAttribute.cs,54
-		/// http://haacked.com/archive/2007/08/21/i-knew-how-to-validate-an-email-address-until-i.aspx/
-		/// </summary>
-		private Regex emailValidator = new Regex (@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", RegexOptions.IgnoreCase);
+		public Camera cam;
 
 		/// <summary>
 		/// Show the keyboard with a custom input message. Attaching events dynamically,
@@ -38,10 +14,10 @@ namespace VRKeys {
 		/// </summary>
 		private void OnEnable () {
 			// Automatically creating camera here to show how
-			GameObject camera = new GameObject ("Main Camera");
-			Camera cam = camera.AddComponent<Camera> ();
-			cam.nearClipPlane = 0.1f;
-			camera.AddComponent<AudioListener> ();
+			//GameObject camera = new GameObject ("Main Camera");
+			//Camera cam = camera.AddComponent<Camera> ();
+			//cam.nearClipPlane = 0.1f;
+			//camera.AddComponent<AudioListener> ();
 
 			// Improves event system performance
 			Canvas canvas = keyboard.canvas.GetComponent<Canvas> ();
@@ -76,18 +52,6 @@ namespace VRKeys {
 					keyboard.Disable ();
 				}
 			}
-
-			if (keyboard.disabled) {
-				return;
-			}
-
-			if (Input.GetKeyDown (KeyCode.Q)) {
-				keyboard.SetLayout (KeyboardLayout.Qwerty);
-			} else if (Input.GetKeyDown (KeyCode.F)) {
-				keyboard.SetLayout (KeyboardLayout.French);
-			} else if (Input.GetKeyDown (KeyCode.D)) {
-				keyboard.SetLayout (KeyboardLayout.Dvorak);
-			}
 		}
 
 		/// <summary>
@@ -103,19 +67,17 @@ namespace VRKeys {
 		public void HandleSubmit (string text) {
 			keyboard.DisableInput ();
 
-			if (!ValidateEmail (text)) {
-				keyboard.ShowValidationMessage ("Please enter a valid email address");
+			if (!ValidateInput (text)) {
+				keyboard.ShowValidationMessage ("Please enter a valid input");
 				keyboard.EnableInput ();
 				return;
 			}
-
-			StartCoroutine (SubmitEmail (text));
 		}
 
 		public void HandleCancel () {
 			Debug.Log ("Cancelled keyboard input!");
 		}
-
+/* 
 		/// <summary>
 		/// Pretend to submit the email before resetting.
 		/// </summary>
@@ -131,12 +93,9 @@ namespace VRKeys {
 			keyboard.HideSuccessMessage ();
 			keyboard.SetText ("");
 			keyboard.EnableInput ();
-		}
+		}*/
 
-		private bool ValidateEmail (string text) {
-			if (!emailValidator.IsMatch (text)) {
-				return false;
-			}
+		private bool ValidateInput (string text) {
 			return true;
 		}
 	}
