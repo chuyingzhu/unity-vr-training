9469aa5d7654b46a15dca75c36bd70638897d948
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-07-05 15:28:26 -0400

    Disable buttons after clicking answer; enable after clicking continue

--- KAT WALK/Assets/Scripts/MCManager.cs
+++ KAT WALK/Assets/Scripts/MCManager.cs
@@ -5,20 +5,38 @@ using UnityEngine.UI;
 
 public class MCManager : MonoBehaviour {
     public Button continueButton;
+    public List<Button> MCButtons;
     private Text buttonText;
 
     private void Awake() {
+        continueButton.GetComponent<Renderer>().enabled = false;
         continueButton.enabled = false;
         buttonText = continueButton.transform.GetChild(0).gameObject.GetComponent<Text>();
     }
 
     public void nextQuestion() {
         buttonText.text = "Next Question";
+        continueButton.GetComponent<Renderer>().enabled = true;
         continueButton.enabled = true;
+        disableMCButtons();
     }
 
     public void tryAgain() {
         buttonText.text = "Try Again";
+        continueButton.GetComponent<Renderer>().enabled = true;
         continueButton.enabled = true;
+        disableMCButtons();
+    }
+
+    private void disableMCButtons() {
+        foreach (Button b in MCButtons) {
+            b.enabled = false;
+        }
+    }
+
+    public void enableMCButtons() {
+        foreach (Button b in MCButtons) {
+            b.enabled = true;
+        }
     }
 }
