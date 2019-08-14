202a215747ad219b3f1eeac2735da2ba722bde7c
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-08-14 17:20:59 -0400

    Add a marker and a script that tells player to walk to the marker

---
+++ KAT WALK/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Called when controller collides with an object
+    private void OnTriggerEnter(Collider other) {
+        // If object is not the type "Marker" simply ignore it
+        if (!other.gameObject.CompareTag("Marker")) {
+            return;
+        }
+        other.gameObject.GetComponent<ColorManager>().changeToGreen();
+    }
+}
