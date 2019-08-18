7ac63644f60a893bb1270b5909fe8bd5f3e4baca
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-08-18 16:00:17 -0400

    Child the UI text to left controller

---
+++ KAT WALK/Assets/Scripts/UIFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFollow : MonoBehaviour {
+	public Text label;
+	public Camera m_Camera;
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 namePos = m_Camera.WorldToScreenPoint(this.transform.position);
+        label.transform.position = namePos;
+    }
+}
