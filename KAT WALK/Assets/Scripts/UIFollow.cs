6ca8703fb59b532e2ca63005a5cf07776aa612dc
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-08-18 23:41:54 -0400

    UI text flips when head turns around

--- KAT WALK/Assets/Scripts/UIFollow.cs
+++ KAT WALK/Assets/Scripts/UIFollow.cs
@@ -7,9 +7,14 @@ using TMPro;
 public class UIFollow : MonoBehaviour {
 	public TextMeshPro text;
 	public Vector3 offset;
+	public Camera m_Camera;
 
     // Update is called once per frame
     void Update() {
         text.transform.position = this.transform.position + offset;
+        // Flip the text if head turns around
+        if (m_Camera.transform.rotation.y > 90 || m_Camera.transform.rotation.y < -90) {
+        	transform.localScale = new Vector3(-1.0f, 0, 0);
+        }
     }
 }
