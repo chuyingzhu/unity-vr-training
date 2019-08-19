1cb4b2481f756bf6267b570f72c48cb0d8a5eb7b
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-08-19 03:02:28 -0400

    UI text flips on start

--- KAT WALK/Assets/Scripts/UIFollow.cs
+++ KAT WALK/Assets/Scripts/UIFollow.cs
@@ -9,11 +9,16 @@ public class UIFollow : MonoBehaviour {
 	public Vector3 offset;
 	public Camera m_Camera;
 
+    // Debugging purpose
+    void Start() {
+        transform.localScale = new Vector3(-1.0f, 0, 0);
+    }
+
     // Update is called once per frame
     void Update() {
         text.transform.position = this.transform.position + offset;
         // Flip the text if head turns around
-        if (m_Camera.transform.rotation.y > 90 || m_Camera.transform.rotation.y < -90) {
+        if (m_Camera.transform.localEulerAngles.y > 90 || m_Camera.transform.localEulerAngles.y < -90) {
             print("Head flipped");
         	transform.localScale = new Vector3(-1.0f, 0, 0);
         }
