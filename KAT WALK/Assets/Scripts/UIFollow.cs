9478ceaafba14170ab0899a5d000eead9f5f700c
Chuying Zhu <c63zhu@uwaterloo.ca>
2019-08-19 21:04:08 -0400

    Text now rotates correctly according to head orientation

--- KAT WALK/Assets/Scripts/UIFollow.cs
+++ KAT WALK/Assets/Scripts/UIFollow.cs
@@ -8,18 +8,16 @@ public class UIFollow : MonoBehaviour {
 	public TextMeshPro text;
 	public Vector3 offset;
 	public Camera m_Camera;
-/*
-    // Debugging purpose
-    void Start() {
-        transform.localScale = new Vector3(-1.0f, 0, 0);
-    }*/
 
     // Update is called once per frame
     void Update() {
         text.transform.position = this.transform.position + offset;
         // Flip the text if head turns around (90 to 270 degrees)
         if (m_Camera.transform.localEulerAngles.y > 90 && m_Camera.transform.localEulerAngles.y < 270) {
-        	text.transform.localScale = new Vector3(-1.0f, 0, 0);
+        	text.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else {
+            text.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
     }
 }
