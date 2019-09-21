fe333b7754ffb9a7845f240c32eaf138fcb5792c
Lightningale <gyl.lakewood@gmail.com>
2019-09-20 20:57:00 -0400

    sync model rotation with kat body

---
+++ KAT WALK/Assets/Scripts/SyncYRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncYRotation : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles = new Vector3(0, target.transform.rotation.y, 0);
+      //  Quaternion x=new Quaternion.euler
+    }
+}
