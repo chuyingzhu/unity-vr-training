82826961b3ef2f5150f0ccdfdb24958eb419a23a
Lightningale <gyl.lakewood@gmail.com>
2019-09-21 20:15:01 -0400

    Unbind models from character root, sync model position

--- KAT WALK/Assets/Scripts/SyncYRotation.cs
+++ KAT WALK/Assets/Scripts/SyncYRotation.cs
@@ -17,9 +17,11 @@ public class SyncYRotation : MonoBehaviour
     {
         newRotation = Quaternion.Euler(0, target.transform.rotation.y, 0);
         this.transform.rotation = newRotation;
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, target.transform.position.z);
        // this.transform.rotation = Quaternion.Euler(0, target.transform.rotation.y, 0);
        // this.transform.eulerAngles = new Vector3(0, target.transform.localRotation.y, 0);
-        Debug.Log("rotationy: "+this.transform.rotation.y+" eulery: "+this.transform.eulerAngles.y);
-      //  Quaternion x=new Quaternion.euler
+        Debug.Log("model rotationy: "+this.transform.rotation.y+" eulery: "+this.transform.eulerAngles.y);
+        Debug.Log("target rotationy: " + target.transform.rotation.y + " eulery: " + target.transform.eulerAngles.y);
+        //  Quaternion x=new Quaternion.euler
     }
 }
