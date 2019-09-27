8b4a6d5092008c9a6f522965cb5c4d350e60277e
Lightningale <gyl.lakewood@gmail.com>
2019-09-27 18:13:33 -0400

    Move interactable to GrabHandleBar

---
+++ KAT WALK/Assets/Scripts/Pulley/GrabHandleBar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHandleBar : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    public Transform target;
+    public Transform handle;
+    public Transform Cart;
+    Rigidbody rb, rb1;
+
+    private void Update()
+    {
+        // If hands move too far away, auto ungrip
+        if(Vector3.Distance(target.position, transform.position) > 0.2f)
+        {
+            OnMouseUp();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        // Store offset
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        // Pixel coor. (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coor of gameObject on screen
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        // Allow dragging of obj
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        // Returning to original position
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+
+        // Resetting object's velocity
+        rb = target.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        rb1 = Cart.GetComponent<Rigidbody>();
+        var heading = target.position - handle.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        if (float.IsNaN(direction.z) || direction.z == 0.0)
+        {
+            rb1.velocity = Vector3.zero;
+            rb1.angularVelocity = Vector3.zero;
+        }
+    }
+
+}
---
+++ KAT WALK/Assets/Scripts/Pulley/Wheel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public bool wheelFrontLeft;
+    public bool wheelFrontRight;
+    public bool wheelRearLeft;
+    public bool wheelRearRight;
+
+    [Header("Suspension")]
+    public float restLength;
+    public float springTravel;
+    public float springStiffness;
+    public float damperStiffness;
+
+    private float minLength;
+    private float maxLength;
+    private float lastLength;
+    private float springLength;
+    private float springForce;
+    private float springVelocity;
+    private float damperForce; 
+
+    private Vector3 suspensionForce;
+
+    [Header("Wheel")]
+    public float wheelRadius;
+    private Vector3 wheelVelocityLS; //Locol space
+    private float Fx;
+    private float Fy;
+
+
+    void Start()
+    {
+        // For the physics
+        rb = transform.root.GetComponent<Rigidbody>();
+
+        minLength = restLength - springTravel;
+        maxLength = restLength + springTravel;
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, -transform.up * (springLength + wheelRadius), Color.green);
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLength + wheelRadius))
+        {
+            lastLength = springLength;
+            springLength = hit.distance - wheelRadius;
+            springLength = Mathf.Clamp(springLength, minLength, maxLength);
+            springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
+            springForce = springStiffness * (restLength - springLength);
+            damperForce = damperStiffness * springVelocity;
+            suspensionForce = (springForce + damperForce) * transform.up;
+            wheelVelocityLS = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point)); // Making sure that the calculation is based on the raycast hit point
+            Fx = Input.GetAxis("Vertical") * springForce;
+            Fy = wheelVelocityLS.x * springForce;
+            rb.AddForceAtPosition(suspensionForce + (Fx * transform.forward) + (Fy * -transform.right), hit.point);  
+
+            
+        }
+    }
+}
