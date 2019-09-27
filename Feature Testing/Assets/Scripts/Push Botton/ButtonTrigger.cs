28887473b92dfbd81e495c65736a036fe7092fed
ElwinUng <elwin2334@gmail.com>
2019-09-27 17:40:23 -0400

    Testing pushable button's physics and scripts (PC only)

---
+++ Feature Testing/Assets/Scripts/Push Botton/ButtonTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    // Event for when triggered
+    public UnityEvent TriggerHit;
+
+    // Detecting if a GameObj with trigger has collided
+    void OnTriggerEnter(Collider obj)
+    {
+        TriggerHit.Invoke();
+    }
+
+    // Make a desired event, HAS TO BE PUBLIC
+    public void Print(string text)
+    {
+        print(text);
+    } 
+}
