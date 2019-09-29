4be25875bd4d0db595149d19fe3f58694ddbe169
ElwinUng <elwin2334@gmail.com>
2019-09-29 02:27:18 -0400

    Pushable Button now display text
    text position and content can be changed in the PrinttoCanvas method

--- Feature Testing/Assets/Scripts/Push Botton/ButtonTrigger.cs
+++ Feature Testing/Assets/Scripts/Push Botton/ButtonTrigger.cs
@@ -5,8 +5,13 @@ using UnityEngine.Events;
 
 public class ButtonTrigger : MonoBehaviour
 {
+    // Floating Text
+    public GameObject TextPrefab;
+    public GameObject parent;
+
     // Event for when triggered
     public UnityEvent TriggerHit;
+    private bool checker = false;
 
     // Detecting if a GameObj with trigger has collided
     void OnTriggerEnter(Collider obj)
@@ -15,8 +20,24 @@ public class ButtonTrigger : MonoBehaviour
     }
 
     // Make a desired event, HAS TO BE PUBLIC
-    public void Print(string text)
+    public void PrinttoCanvas(string text)
     {
-        print(text);
+        Debug.Log("Hit");
+        if (checker == false)
+        {
+            var textMesh = Instantiate(TextPrefab, new Vector3(-0.03f, 1.53f, -6.108f), Quaternion.identity, parent.transform);
+            textMesh.GetComponent<TextMesh>().text = "ON";
+            Destroy(textMesh, 1.5f);
+            checker = true;
+            Debug.Log("True");
+        }
+        else if (checker == true)
+        {
+            var textMesh = Instantiate(TextPrefab, new Vector3(-0.03f, 1.53f, -6.108f), Quaternion.identity, parent.transform);
+            textMesh.GetComponent<TextMesh>().text = "OFF";
+            Destroy(textMesh, 1.5f);
+            checker = false;
+            Debug.Log("False");
+        }
     } 
 }
