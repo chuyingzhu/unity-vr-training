3f4ca46c6b3d541dfcba1917af6d71d05260362f
Lightningale <gyl.lakewood@gmail.com>
2019-09-30 16:26:36 -0400

    change grabhandlebar script

--- KAT WALK/Assets/Scripts/Pulley/GrabHandleBar.cs
+++ KAT WALK/Assets/Scripts/Pulley/GrabHandleBar.cs
@@ -1,46 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class GrabHandleBar : MonoBehaviour
 {
-    private Vector3 mOffset;
-    private float mZCoord;
+    // PC Debugging only
+    /*private Vector3 mOffset;
+    private float mZCoord;*/
     public Transform target;
     public Transform handle;
     public Transform Cart;
+    public SteamVR_Action_Boolean grabPinch;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
     Rigidbody rb, rb1;
 
+    private void Awake()
+    {
+        grabPinch = SteamVR_Actions._default.GrabPinch;
+    }
     private void Update()
     {
         // If hands move too far away, auto ungrip
-       // if(Vector3.Distance(target.position, transform.position) > 20.0f)
-      //  {
-      //      OnMouseUp();
-      //  }
+        if(Vector3.Distance(target.position, transform.position) > 0.2f)
+        {
+            //OnMouseUp(); //PC Debugging only
+            ReturntoInitPos();
+        }
     }
 
-    private void OnMouseDown()
+    private void FixedUpdate()
+    {
+        if (grabPinch.GetStateUp(inputSource))
+        {
+            ReturntoInitPos();
+        }
+    }
+
+
+    private void ReturntoInitPos()
+    {
+        // Returning to original position
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+
+        // Resetting object's velocity
+        rb = target.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        rb1 = Cart.GetComponent<Rigidbody>();
+        var heading = target.position - handle.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        if (float.IsNaN(direction.z) || direction.z == 0.0)
+        {
+            rb1.velocity = Vector3.zero;
+            rb1.angularVelocity = Vector3.zero;
+        }
+    }
+
+    // PC Debugging only
+    /*
+    void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-
         // Store offset
         mOffset = gameObject.transform.position - GetMouseWorldPos();
     }
-
+    
     private Vector3 GetMouseWorldPos()
     {
-        // Pixel coor. (x,y)
-        Vector3 mousePoint = Input.mousePosition;
+        // Pixel coord.
+        Vector3 mousePoint = Input.mousePosition();
 
-        // z coor of gameObject on screen
+        //z coord. of gameObject on screen
         mousePoint.z = mZCoord;
+
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
     private void OnMouseDrag()
     {
-        // Allow dragging of obj
         transform.position = GetMouseWorldPos() + mOffset;
     }
 
@@ -65,5 +106,6 @@ public class GrabHandleBar : MonoBehaviour
             rb1.angularVelocity = Vector3.zero;
         }
     }
+    */
 
 }
